425425478cd097676b2d9f1833686e6763a45e75
RAJ-JAKHAD <106586420+RAJ-JAKHAD@users.noreply.github.com>
2022-05-31 22:41:00 +0530

    Create 05_randome_choices.py

---
+++ MISCELLENOUS/05_randome_choices.py
@@ -0,0 +1,6 @@
+import random as r
+#it return a list of randome selected element from the given specified list.
+# k is parameter in k we pass the size .
+a=["ram","sita","radhe","krishna"]
+n=r.choices(a,k=8)
+print(n)
