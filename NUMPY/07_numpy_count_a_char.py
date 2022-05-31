a7903a9058769edade92a5fab1199eb255bda539
RAJ-JAKHAD <106586420+RAJ-JAKHAD@users.noreply.github.com>
2022-05-31 15:47:48 +0530

    Create 07_numpy_count_a_char.py

---
+++ NUMPY/07_numpy_count_a_char.py
@@ -0,0 +1,8 @@
+import numpy as np
+'''a=np.array(['ayush','pandey','pppp'],dtype=str)
+b=np.char.count(a,'p')
+print(b)'''
+
+a=np.array(['1234','2111','1'],dtype=str)
+b=np.char.count(a,'1')
+print(b)
